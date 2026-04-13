a4bcb173a453b9f18b7dd88218af6fb89578c812
Hung Le <hung.le@optimziely.com>
2026-04-13 15:58:14 +0700

    Enhance find-file tool to support relative path resolution and improve error handling for better user experience.

---
+++ mcp/client/sampling/index.ts
@@ -0,0 +1,68 @@
+import { GroqChatModelId } from "@/types/groq";
+import { groq } from "@ai-sdk/groq";
+import { Client } from "@modelcontextprotocol/client";
+import {
+  stepCountIs,
+  generateText,
+  UIMessageStreamWriter,
+  ModelMessage,
+} from "ai";
+
+export const registerClientSamplingHandlers = (
+  client: Client,
+  writer: UIMessageStreamWriter,
+) => {
+  console.log("Registering client sampling handlers...");
+  client.setRequestHandler("sampling/createMessage", async (request) => {
+    const messageFromServer = request.params.messages.at(-1);
+
+    if (!messageFromServer) {
+      throw new Error("No message from server");
+    }
+
+    let content: string = "";
+
+    if (Array.isArray(messageFromServer.content)) {
+      messageFromServer.content.forEach((part) => {
+        if (part.type === "text") {
+          content += part.text;
+        }
+      });
+    } else if (messageFromServer.content.type === "text") {
+      content = messageFromServer.content.text;
+    }
+
+    if (!content) {
+      throw new Error("No content in message from server");
+    }
+
+    const message: ModelMessage = {
+      role: messageFromServer?.role || "user",
+      content,
+    };
+
+    console.log("Sampling request from server:", messageFromServer);
+    console.log("Sampling message to LLM:", message);
+
+    console.log("send sampling message to LLM");
+
+    const result = await generateText({
+      model: groq(GroqChatModelId.GPT_OSS_120B),
+      messages: [message],
+      stopWhen: stepCountIs(5),
+      system: "You are a helpful assistant that can answer questions",
+    });
+
+    console.log("Sampling result from LLM:", result.text);
+
+    return {
+      model: GroqChatModelId.GPT_OSS_120B,
+      role: "assistant",
+      content: {
+        type: "text",
+        text: result.text,
+      },
+    };
+  });
+  console.log("Client sampling handlers registered");
+};
